4a4b26f72d4eb6f09282fef2ef626abf476aa367
Ben Watherston <ben@cleversteam.com>
2018-04-19 11:23:55 +0100

    Homepage block

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
   document.getElementById('cookie__close').addEventListener("click", function() {
     document.getElementById('cookie__wrap').style.display = "none";
   }, false);
+
+  document.getElementById('speech--open').addEventListener("click", function() {
+    document.getElementById('speech--wrap').classList.add("active");
+  }, false);
+
+  document.getElementById('speech--close').addEventListener("click", function() {
+    document.getElementById('speech--wrap').classList.remove("active");
+  }, false);
