8f13d1d998ec17bd5d846d8fcf9ae9e7d2480bab
Ben Watherston <ben@cleversteam.com>
2018-05-08 11:40:09 +0100

    Mass update

--- gulpfile.js
+++ gulpfile.js
@@ -6,11 +6,14 @@ const uglify = require("gulp-uglify");
 gulp.task("es6", () => {
   return gulp
     .src([
+      "javascripts/vendor/jquery-3.3.1.js",
+      "javascripts/vendor/slick.js",
       "src/functions.js",
       "src/main.js",
       "src/navigation.js",
       "src/contact-modal.js",
-      "src/cookie.js"
+      "src/cookie.js",
+      "src/day-slider.js"
     ])
     .pipe(concat("main.js"))
     .pipe(
---
+++ src/day-slider.js
@@ -0,0 +1,14 @@
+const daySlider = document.querySelector("#day_slider");
+
+// Run on page after slick slider
+const updateRangeSlider = () => {
+  const getSlides = document.querySelectorAll(".slick-slide");
+  daySlider.max = getSlides.length - 1;
+};
+
+// Search - hide search text when text is entered
+const goToSlider = () => {
+  daySlider.addEventListener("input", function(e) {
+    return e.target.value;
+  });
+};
