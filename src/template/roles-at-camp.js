60a479873c68403b2e03f092dd88b0da163a992f
Ben Watherston <ben@cleversteam.com>
2018-05-22 16:02:23 +0100

    Add custom slider dots

--- src/template/roles-at-camp.js
+++ src/template/roles-at-camp.js
@@ -5,7 +5,66 @@ $(function() {
     slidesToScroll: 1,
     prevArrow: $(".slick-prev"),
     nextArrow: $(".slick-next"),
-    lazyLoad: "ondemand",
-    dots: true
+    lazyLoad: "progressive",
+    adaptiveHeight: true
+  });
+});
+
+const sliderWrap = document.querySelectorAll(".slider__dots--wrap");
+
+$(".block__slider--wrap").on("init", function(
+  event,
+  slick,
+  currentSlide,
+  nextSlide
+) {
+  sliderWrap.forEach((dotsWrap, index) => {
+    let createDot = index => {
+      let node = document.createElement("span");
+      node.classList.add("slider__dots--item");
+      node.setAttribute("data-slide", index);
+      if (index == 0) {
+        node.classList.add("active");
+      }
+      dotsWrap.appendChild(node);
+    };
+    for (var index = 0; index < slick.slideCount; index++) {
+      createDot(index);
+    }
+
+    const allDots = document.querySelectorAll(".slider__dots--item");
+
+    allDots.forEach(singleDot => {
+      singleDot.addEventListener(
+        "click",
+        function() {
+          $(".block__slider--wrap").slick(
+            "slickGoTo",
+            this.getAttribute("data-slide"),
+            0
+          );
+        },
+        false
+      );
+    });
+  });
+});
+
+$(".block__slider--wrap").on("afterChange", function(
+  event,
+  slick,
+  currentSlide,
+  nextSlide
+) {
+  let curSlide = currentSlide ? currentSlide : 0;
+  let allDots = document.querySelectorAll(".slider__dots--item");
+
+  allDots.forEach(dot => {
+    dot.classList.remove("active");
+  });
+
+  let active = document.querySelectorAll(`[data-slide="${curSlide}"]`);
+  active.forEach(a => {
+    a.classList.add("active");
   });
 });
